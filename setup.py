17d00b9516105299a5834a76e88fbb5ac5151848
Rafael Capucho <rafael.capucho@gmail.com>
2016-06-10 03:34:01 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+
+LONG_DESC = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+
+setup(name='scrapy-eagle',
+    version='0.0.1',
+    description='Run Scrapy Distributed',
+    long_description=LONG_DESC,
+    author='Rafael Alfredo Capucho',
+    author_email='rafael.capucho@gmail.com',
+    url='http://github.com/rafaelcapucho/scrapy-eagle',
+    packages=['scrapy_eagle.worker', 'scrapy_eagle.dashboard'],
+    license='BSD',
+    install_requires=['Scrapy>=1.1.0', 'redis>=2.10.0'],
+    entry_points = {
+        'console_scripts': ['eagle_server=scrapy_eagle.dashboard.server:main'],
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Scrapy',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Intended Audience :: Developers',
+    ],
+)
