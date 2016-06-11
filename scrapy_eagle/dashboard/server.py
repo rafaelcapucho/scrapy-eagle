4da556345fb6926df8d51ddc45e0ea50c6041afb
Rafael Capucho <rafael.capucho@gmail.com>
2016-06-11 19:22:42 -0300

    Hability to set redis connection

--- scrapy_eagle/dashboard/server.py
+++ scrapy_eagle/dashboard/server.py
@@ -1,4 +1,37 @@
+import argparse
+
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+import redis
+
+redis_pool, redis_conn = (None, None)
+
+
 def main():
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-c', '--config-file', help='Config file path.')
+
+    args = parser.parse_args()
+
+    if not args.config_file:
+        print('You should specify a config file using --config-file parameter.')
+        exit(0)
+
+    config = configparser.ConfigParser()
+    config.read(args.config_file)
+
+    redis_pool = redis.ConnectionPool(
+        host=config['redis']['host'],
+        port=config['redis']['port'],
+        db=config['redis']['db']
+    )
+
+    redis_conn = redis.Redis(connection_pool=redis_pool)
+
     print("Executing the server...")
 
 if __name__ == "__main__":
