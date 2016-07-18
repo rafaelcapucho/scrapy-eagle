6319cb64bd0eba781226c4661949dfcbdc6a0cfd
Rafael Capucho <rafael.capucho@gmail.com>
2016-07-18 20:26:05 -0300

    embedding flask within the server

--- scrapy_eagle/dashboard/server.py
+++ scrapy_eagle/dashboard/server.py
@@ -1,13 +1,30 @@
+from gevent import monkey
+monkey.patch_all()
+
+import sys
+import signal
 import argparse
+import threading
+
+import redis
+import flask
+import gevent
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit
+
+from scrapy_eagle.dashboard.utils import ip
 
 try:
     import configparser
 except ImportError:
     import ConfigParser as configparser
 
-import redis
+app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = 'ha74%ahtus342'
+app.config['DEBUG'] = True
+CORS(app)
 
-from scrapy_eagle.dashboard.utils import ip
+socketio = SocketIO(app, async_mode='gevent')
 
 redis_pool, redis_conn = (None, None)
 
@@ -44,5 +61,30 @@ def main():
 
     print("Executing the server...")
 
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+def shutdown():
+
+    print('\nshutting down {0}...'.format(threading.currentThread().getName()))
+
+    sys.exit(0)
+
 if __name__ == "__main__":
+
+    # Graceful shutdown when kill are received
+    signal.signal(signal.SIGTERM, lambda sig, frame: shutdown())
+
+    # Graceful shutdown when terminal session are closed
+    signal.signal(signal.SIGHUP, lambda sig, frame: shutdown())
+
     main()
+
+    try:
+
+        # use_reloader: avoid Flask execute twice
+        socketio.run(app, host='0.0.0.0', port=5001, use_reloader=False)
+
+    except (KeyboardInterrupt, SystemExit):
+        shutdown()
