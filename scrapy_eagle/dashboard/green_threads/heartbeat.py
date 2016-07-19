f8cf340c4f7b6dda271faeca5e66873d14a5e4f9
Rafael Capucho <rafael.capucho@gmail.com>
2016-07-19 00:08:50 -0300

    added heartbeat_subprocess

--- scrapy_eagle/dashboard/green_threads/heartbeat.py
+++ scrapy_eagle/dashboard/green_threads/heartbeat.py
@@ -1,6 +1,6 @@
 import os
 import signal
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta
 
 import gevent
 
@@ -21,3 +21,37 @@ def heartbeat_servers(redis_conn, ip, hostname):
         servers = redis_conn.zrangebyscore('servers', now.timestamp(), max='+inf')
 
         gevent.sleep(3)
+
+def heartbeat_subprocess(pid, spider, max_seconds_idle, max_size_limit, queue_info_global):
+
+    last_time_zero = datetime.now()
+    last_processed = None
+
+    max_size = 0
+
+    while True:
+
+        size = None
+        for entry in queue_info_global:
+            if entry['name'] == spider:
+                size = entry['size']
+
+        if size > 0:
+            last_processed = datetime.now()
+
+        if size > max_size:
+            max_size = size
+
+        if last_processed:
+            diff = datetime.now() - last_processed
+
+            # print('\nlast_processed_secs: ', diff.seconds, ' maxsize: ', max_size, ' size: ', size, '\n\n')
+
+            if diff.seconds > max_seconds_idle and max_size > max_size_limit:
+
+                os.kill(pid, signal.SIGHUP)
+
+                break
+
+
+        gevent.sleep(2)
