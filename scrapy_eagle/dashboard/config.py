cc75dbe40385df0e16b2fc0e0769720b71b7a24a
Rafael Capucho <rafael.capucho@gmail.com>
2016-07-18 23:09:23 -0300

    add underscore prefix to private variable

--- scrapy_eagle/dashboard/config.py
+++ scrapy_eagle/dashboard/config.py
@@ -7,31 +7,31 @@ except ImportError:
 
 # Never import these directly
 # Use get_config and get_args instead
-args = None
-config = None
+_args = None
+_config = None
 
 def setup():
 
-    global args, config
+    global _args, _config
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-c', '--config-file', help='Config file path.')
 
-    args = parser.parse_args()
+    _args = parser.parse_args()
 
-    if not args.config_file:
+    if not _args.config_file:
         print('You should specify a config file using --config-file parameter.')
         exit(0)
 
-    config = configparser.ConfigParser()
-    config.read(args.config_file)
+    _config = configparser.ConfigParser()
+    _config.read(_args.config_file)
 
-    globals()['config'] = config
+    globals()['config'] = _config
 
-    return args, config
+    return _args, _config
 
 def get_config():
-    return config
+    return _config
 
 def get_args():
-    return args
+    return _args
