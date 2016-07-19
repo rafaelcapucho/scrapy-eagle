4f6874814993be70b9accf113f6237d746648436
Rafael Capucho <rafael.capucho@gmail.com>
2016-07-18 23:41:24 -0300

    updated config to hold ip and hostname

--- scrapy_eagle/dashboard/config.py
+++ scrapy_eagle/dashboard/config.py
@@ -5,14 +5,18 @@ try:
 except ImportError:
     import ConfigParser as configparser
 
+from scrapy_eagle.dashboard.utils import ip
+
 # Never import these directly
 # Use get_config and get_args instead
 _args = None
 _config = None
+_public_ip = None
+_hostname = None
 
 def setup():
 
-    global _args, _config
+    global _args, _config, _public_ip, _hostname
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-c', '--config-file', help='Config file path.')
@@ -28,7 +32,21 @@ def setup():
 
     globals()['config'] = _config
 
+    print('discovering your external entrypoint address... ', end='', flush=True)
+
+    _public_ip = ip.get_external_ip()
+
+    print(_public_ip)
+
+    _hostname = ip.get_hostname()
+
     return _args, _config
+
+def get_public_ip():
+    return _public_ip
+
+def get_hostname():
+    return _hostname
 
 def get_config():
     return _config
