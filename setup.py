04ad4e268c2478d2d0ea3636471d7f006abbf152
Rafael Capucho <rafael.capucho@gmail.com>
2016-07-22 02:36:37 -0300

    PEP8 Fix

--- setup.py
+++ setup.py
@@ -7,13 +7,16 @@ from setuptools import setup, find_packages
 
 LONG_DESC = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
+
 def read_file(filename):
     with io.open(filename) as fp:
         return fp.read().strip()
 
+
 def read_requirements(filename):
     return [line.strip() for line in read_file(filename).splitlines()
         if not line.startswith('#')]
+
 
 setup(name='scrapy-eagle',
     version='0.0.17',
@@ -25,7 +28,7 @@ setup(name='scrapy-eagle',
     packages=find_packages(),
     license='BSD',
     install_requires=read_requirements('requirements.txt'),
-    entry_points = {
+    entry_points={
         'console_scripts': ['eagle_server=scrapy_eagle.dashboard.main:entry_point'],
     },
     classifiers=[
