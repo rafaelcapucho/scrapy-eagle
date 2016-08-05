53107810c3fa140b104227de294705cb73aa13f2
Rafael Capucho <rafael.capucho@gmail.com>
2016-08-05 03:40:05 -0300

    Move the HTML code to JSX code

--- scrapy_eagle/dashboard/react-src/components/App.jsx
+++ scrapy_eagle/dashboard/react-src/components/App.jsx
@@ -9,13 +9,70 @@ export default class App extends React.Component {
   render(){
     return (
       <div>
-        <ul>
-          <li><IndexLink to="/app/" activeClassName="active">/</IndexLink></li>
-          <li><Link to="/app/servers/monitoring" activeClassName="active">/servers/monitoring</Link></li>
-          <li><Link to="/app/spiders/config" activeClassName="active">/spiders/config</Link></li>
-        </ul>
 
-        {this.props.children}
+        <div className="container-fluid subheader">
+          Dashboard >
+        </div>
+
+        <div className="flexbox">
+
+          <section className="main-content-wrapper">
+
+            <h1>Distributed Scrapy</h1>
+            <h2>Monitoring Dashboard</h2>
+
+            <ul>
+              <li><IndexLink to="/app/" activeClassName="active">/</IndexLink></li>
+              <li><Link to="/app/servers/monitoring" activeClassName="active">/servers/monitoring</Link></li>
+              <li><Link to="/app/spiders/config" activeClassName="active">/spiders/config</Link></li>
+            </ul>
+
+            {this.props.children}
+
+          </section>
+
+          <aside className="sidebar sidebar-left">
+
+            <nav>
+              <h5 className="sidebar-header">Navigation</h5>
+              <ul className="nav nav-pills nav-stacked">
+
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Option 1
+                    <span className="pull-right tag tag-pill tag-primary">8</span>
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Option 2
+                    <span className="pull-right tag tag-danger">new</span>
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link" title="Dashboard" href="#">Dashboard</a>
+                </li>
+
+                <li className="nav-item nav-dropdown">
+                  <a className="nav-link active" title="Dashboard 2" href="#">
+                    Dashboard Drop
+                  </a>
+                  <ul className="nav-sub" data-index="0" style={{display: 'none'}}>
+                    <li><a title="Buttons" href="#"> Buttons </a></li>
+                    <li className="active"><a title="Buttons" href="#"> Buttons </a></li>
+                    <li><a title="Buttons" href="#"> Buttons </a></li>
+                    <li><a title="Buttons" href="#"> Buttons </a></li>
+                  </ul>
+                </li>
+              </ul>
+
+            </nav>
+
+          </aside>
+
+        </div>
 
       </div>
       );
