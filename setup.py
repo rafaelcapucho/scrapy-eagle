5704c6c9e045bf2cc5a5f22061da11d12e22b6b1
Rafael Capucho <rafael.capucho@gmail.com>
2016-08-23 18:24:09 -0300

    Bumb version to 0.0.30

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read_requirements(filename):
 
 
 setup(name='scrapy-eagle',
-    version='0.0.29',
+    version='0.0.30',
     description='Run Scrapy Distributed',
     long_description=LONG_DESC,
     author='Rafael Alfredo Capucho',
